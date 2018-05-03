b725cc4caef8090d616c5f19c837746e1a44097e
Tomas <dev.tomas.ivan@gmail.com>
2018-05-03 21:40:31 +0200

    init

---
+++ app/src/main/java/sk/itti/playground/websocketdemo/DemoActivity.kt
@@ -0,0 +1,12 @@
+package sk.itti.playground.websocketdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+    }
+}
